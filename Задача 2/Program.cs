b59e157945b9c4bf7c0f3251df39897fc7523280
podolski133 <podolski133@gmail.com>
2022-04-16 17:48:04 +0300

    add task 2, 4

---
+++ Задача 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+int numberA, numberB;
+int max;
+
+numberA = 5;
+numberB = 7;
+
+max = numberA;
+
+if (max > numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+Console.WriteLine(max);
---
+++ Задача 4/Program.cs
@@ -0,0 +1,26 @@
+﻿int numberA, numberB, numberC;
+int max;
+
+numberA = 44;
+numberB = 5;
+numberC = 78;
+
+max = numberA;
+
+if (max > numberB)
+{
+    max = numberA;
+}
+else
+{
+    max = numberB;
+}
+if (max > numberC)
+{
+    max = numberA;
+}
+else
+{
+    max = numberC;
+}
+System.Console.WriteLine(max);
