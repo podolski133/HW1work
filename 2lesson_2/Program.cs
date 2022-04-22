c3376374458cd9a678b73fdea311ef9d5c1a896a
podolski133 <podolski133@gmail.com>
2022-04-22 22:05:59 +0300

    add 2lesson_2

---
+++ 2lesson_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2: Написать программу, которая выводит случайное
+// трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+
+int number = new Random().Next(100, 1000);
+int newnumber = 0;
+int NumberWithoutSecDig()
+{
+    newnumber = number / 100 * 10 + number % 10;
+    return newnumber;
+}
+NumberWithoutSecDig();
+Console.WriteLine($"Из числа {number} удаляем вторую цифру, и получаем {newnumber}");
