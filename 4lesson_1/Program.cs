eaafcce8387ef5a9f75fa6b416b7f819d00f5724
podolski133 <podolski133@gmail.com>
2022-05-03 19:20:54 +0300

    add 4lesson_1

---
+++ 4lesson_1/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 25: Напишите (функцию) цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16  */
+
+int A = 2;
+int B = 4;
+int count = 1;
+int result = A;
+
+while (count < B)
+{
+    result = result * A;
+    count++;
+}
+Console.WriteLine($"{A} ^ {B} = {result}");
