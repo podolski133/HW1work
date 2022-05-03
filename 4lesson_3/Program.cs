d7c87517cf747c3667a831264e03eb0b705d4c7c
podolski133 <podolski133@gmail.com>
2022-05-03 20:14:42 +0300

    add 4lesson_3

---
+++ 4lesson_3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из некоторого 
+количества элементов и выводит их на экран с помощью функций. 
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int N = new Random().Next(1, 100);
+
+int[] SetArray(int x)
+{
+    int[] arr = new int[x];
+    for (int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        System.Console.Write($"{array[i]} ");
+}
+
+PrintArray(SetArray(N));
