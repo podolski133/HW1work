e83dc901d2617e76d050518a822deb9884e5693c
podolski133 <podolski133@gmail.com>
2022-05-03 20:00:27 +0300

    add 4lesson_2

---
+++ 4lesson_2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 27: Напишите функцию, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int number = new Random().Next(1, 10000);
+int sum = 0;
+Console.Write($"Сумма цифр числа {number} равна ");
+while (number > 0)
+{
+    int sum1 = number % 10;
+    sum = sum + sum1;
+    number = number / 10;
+}
+Console.WriteLine(sum);
