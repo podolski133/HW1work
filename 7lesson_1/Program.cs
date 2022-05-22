efd03998538a67a2b2da487c5cda55361e9314e5
podolski133 <podolski133@gmail.com>
2022-05-22 18:46:19 +0300

    add 7lesson_1,2,3

---
+++ 7lesson_1/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+double[,] GetTableDouble(int m, int n)
+{
+    double[,] table = new double[m, n];
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().NextDouble() * 100;
+        }
+    }
+    return table;
+}
+
+void PrintTable(double[,] tab)
+{
+    for (int i = 0; i < tab.GetLength(0); i++)
+    {
+        for (int j = 0; j < tab.GetLength(1); j++)
+        {
+            Console.Write(tab[i, j] + "    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = 3;
+int n = 4;
+double[,] TableDouble = GetTableDouble(m, n);
+PrintTable(TableDouble);
---
+++ 7lesson_2/Program.cs
@@ -0,0 +1,87 @@
+﻿/* Задача 50: Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, и возвращает значение этого элемента или же указание, 
+что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+1 7 -> такого числа в массиве нет
+*/
+
+int[,] CreateArray(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+int[] FindNumberPosition(int[,] arr, int number)
+{
+    int[] position = new int[2];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == number)
+            {
+                position[0] = i + 1;
+                position[1] = j + 1;
+                return position;
+            }
+        }
+    }
+    position[0] = -1;
+    position[1] = -1;
+    return position;
+}
+
+void PrintArray(int[,] array)
+{
+    Console.WriteLine("Массив:");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 1000 > 0) Console.Write($"{array[i, j]}   ");
+            else if (array[i, j] / 100 > 0) Console.Write($" {array[i, j]}   ");
+            else if (array[i, j] / 10 > 0) Console.Write($"  {array[i, j]}   ");
+            else Console.Write($"   {array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintPosition(int[] pos, int num)
+{
+    Console.WriteLine();
+    if (pos[0] > 0 && pos[1] > 0) Console.WriteLine($"Число {num} находится в {pos[0]}-й строке, {pos[1]}-м столбце");
+    else Console.WriteLine($"Число {num} отсутствует в заданном массиве");
+    Console.WriteLine();
+}
+
+int GetNumberToFind()
+{
+    Console.WriteLine();
+    Console.Write("Введите число:   ");
+    string writeNumber = Console.ReadLine();
+    int number = Convert.ToInt32(writeNumber);
+    return number;
+}
+
+int row = 5;
+int col = 5;
+int min = 1;
+int max = 1000;
+
+int[,] array1 = CreateArray(row, col, min, max);
+PrintArray(array1);
+int number = GetNumberToFind();
+int[] position = FindNumberPosition(array1, number);
+PrintPosition(position, number);
