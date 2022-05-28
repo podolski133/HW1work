a53d2d0c20f7f7ee5a8f867e53e2af13b43f767a
podolski133 <podolski133@gmail.com>
2022-05-28 14:44:33 +0300

    add 8lesson_1

---
+++ 8lesson_1/Program.cs
@@ -0,0 +1,65 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая 
+упорядочит по убыванию элементы каждой строки двумерного массива.
+6 4 1 9
+4 9 1 2
+->
+9 6 4 1
+9 4 2 1
+*/
+
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int[,] SortRowAscending(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;                                            
+            for (int k = j + 1; k < array.GetLength(1); k++)                
+            {
+                if (array[i, k] > array[i, minPosition]) minPosition = k;    
+            }
+            int tmp = array[i, j];                                          
+            array[i, j] = array[i, minPosition];                            
+            array[i, minPosition] = tmp;                                    
+        }
+
+    }
+    return array;
+}
+
+void PrintTable(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = 4;
+int col = 5;
+int min = 10;
+int max = 100;
+
+int[,] table = GetMatrix(row, col, min, max);
+PrintTable(table);
+
+Console.WriteLine();
+int[,] rowSort = SortRowAscending(table);
+PrintTable(rowSort);
