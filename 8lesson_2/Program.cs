4b173d4f1f3ac123259aa79dcbd45ac4984f9575
podolski133 <podolski133@gmail.com>
2022-05-28 15:08:27 +0300

    add 8lesson_2

---
+++ 8lesson_2/Program.cs
@@ -0,0 +1,65 @@
+﻿/* Задача 56: Задайте прямоугольный двумерный массив. Напишите 
+программу, которая будет находить строку с наименьшей суммой элементов.
+9 9 9
+1 1 1
+5 6 7
+-> вторая строка
+*/
+
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] array = new int[row, col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+int rowNumber(int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        if (i == 0) result = sum;
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;
+        }
+    }
+    return index;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = 5;
+int col = 5;
+int min = 1;
+int max = 10;
+
+int[,] matrix = GetMatrix(row, col, min, max);
+PrintMatrix(matrix);
+Console.WriteLine("---");
+int index = rowNumber(matrix);
+Console.WriteLine($"Строка с наименьшей суммой элементов: {index}");
